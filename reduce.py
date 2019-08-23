fa256f82f4e9be4a0bc282e95aaeff9c3ff71ee7
Bryan Glazer <bryan.glazer@vanderbilt.edu>
2019-08-22 19:21:28 -0500

    Working but not effective

--- reduce.py
+++ reduce.py
@@ -3,69 +3,96 @@ from torch.nn import Sequential as Seq, Linear, ReLU, BatchNorm1d as BN
 from sklearn.neighbors import BallTree, DistanceMetric
 from torch import tensor, zeros
 from torchvision import datasets
+from torchvision import transforms
+from torch.autograd import Variable
+import numpy as np
 
 def MLP(channels):
     return Seq(*[
         Seq(
         Linear(channels[i - 1], channels[i]), 
-        ReLU(), 
-        BN(channels[i]))
+        ReLU()
+        )
+        #, BN(channels[i]))
         for i in range(1, len(channels))
     ])
 
+def squared_euclidean(x, y):
+    return np.sum((x-y)**2)
+
 class Reducer(torch.nn.Module):
     def __init__(self, data, output_size, num_neighbors=100, metric_p=2):
+        super(Reducer, self).__init__()
         # TODO this probably wont work
-        rows, cols = data.shape
+        self.rows, self.cols = data.shape
         # TODO layer sizes?
         # TODO dropout?
-        self.mlp = MLP([input_size, 128, 64, 32, output_size])
+        self.mlp = MLP([self.cols, 128, 64, 32])
+        self.out_layer = Linear(32, output_size)
         self.data = data
-        self.balltree = BallTree(self.data)
-        self.metric = DistanceMetric.get_metric('minkowski',metric_p))
+        self.balltree = BallTree(self.data, metric=squared_euclidean)
         self.num_neighbors=num_neighbors
 
-        self.neighbor_dists = zeros([rows, num_neighbors])
-        self.idxs = []
-        for i,point in enumerate(self.data):
-            dists, idxs = balltree.query(self.data[i,:], k=self.num_neighbors)
-            self.neighbor_dists[i,:] = dists
-            self.idxs.append(idxs)
+        dists, self.idxs = self.balltree.query(self.data, k=self.num_neighbors)
+        self.true_distances = torch.as_tensor(dists,dtype=torch.float32)
+        self.true_distances = self.true_distances.reshape(-1,1)
         
-    def neighbor_loss(self, output):
-        loss = 0.0
+    def forward(self):
+        projected = self.mlp(self.data)
+        projected = self.out_layer(projected)
+
+        proj_dists = zeros([self.rows*self.num_neighbors,1])
+
+        k=0
         for i,idxs in enumerate(self.idxs):
             # proj = projected, i.e. a point in the reduced dimensionality space
             # orig = original i.e. a point in the original dimensionality
-            orig_neighbors = self.data[idxs,:]
-            proj_neighbors = output[idxs,:]
+            proj_neighbors = projected[idxs,:]
 
-            orig_point = self.data[i,:]
-            proj_point = output[i,:]
+            proj_point = projected[i,:]
 
-            for i in range(self.num_neighbors):
-                orig_neighbor = orig_neighbors[i]
-                proj_neighbor = proj_neighbors[i]
-                orig_dist = orig_point - orig_neighbor
-                proj_dist = proj_point - proj_neighbor
-                loss += (orig_dist - proj_dist)**2
+            for j in range(self.num_neighbors):
+                proj_neighbor = proj_neighbors[j]
+                # TODO something to keep torch from calculating the gradient for 
+                # both the point and its neighbor???
+                #proj_dist = torch.sqrt(torch.sum((proj_point - proj_neighbor)**2))
+                proj_dist = torch.sum((proj_point - proj_neighbor)**2)
+                proj_dists[k] = proj_dist
+                k+=1
 
-        return loss
-    
-    def forward(self, data):
-        projected = self.mlp(data)
-        return projected
+        return proj_dists
 
-data = datasets.MNIST(root='./data'
+data = datasets.MNIST(root='./data',
                       train=True,
-                      transform=transformers.ToTensor(),
+                      transform=transforms.ToTensor(),
                       download=True)
+data = data.data
+data = data.type(torch.FloatTensor)
+data = Variable(data.view(-1, 28*28))
+#loader = torch.utils.data.DataLoader(dataset=data, 
+#                                     #batch_size=batch_size, 
+#                                     shuffle=True)
 
-loader = torch.utils.data.DataLoader(dataset=train_dataset, 
-                                     batch_size=batch_size, 
-                                     shuffle=True)
+epochs = 200 
 
-reducer = Reducer(
-self.optimizer = torch.optim.Adam(
+num_points = 1000
+num_neighbors = 10
+reducer = Reducer(data.data[:num_points],
+                  output_size=2, 
+                  num_neighbors=num_neighbors,
+                  metric_p=2)
 
-for 
+optimizer = torch.optim.Adam(reducer.parameters())#, lr=1e-6)
+
+mse = torch.nn.MSELoss()
+
+for epoch in range(epochs):
+    print(f'epoch: {epoch}')
+    optimizer.zero_grad()
+    projected_distances = reducer.forward()
+    loss = mse(projected_distances, reducer.true_distances)
+    loss.backward()
+    #torch.nn.utils.clip_grad_value_(reducer.parameters(), 10)
+    optimizer.step()
+    print(loss)
+
