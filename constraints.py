4deb8e3c908c7b2a1554632cc6c761a5f630224a
Bryan Glazer <bryan.glazer@vanderbilt.edu>
2019-09-11 18:00:28 -0500

    Adding sklearn distance calculation, distance calculation

--- constraints.py
+++ constraints.py
@@ -13,6 +13,7 @@ from sklearn.neighbors import BallTree, DistanceMetric
 # load data
 num_points = 400
 num_neighbors = 10
+dim = 2
 #sample_size = 50
 
 data = datasets.MNIST(root='./data',
@@ -25,17 +26,34 @@ data = data.type(torch.FloatTensor)
 data = Variable(data.view(-1, 28*28))
 data = data[:num_points]
 npdata = data.numpy()
+#data.requires_grad = True
 #data = Data(pos=data)
 #data = Batch(Data(x=data))
 
 # compute knn graph
-#knn = KNNGraph(k=num_neighbors)
-#g = knn(data)
 def squared_euclidean(x, y):
     return np.sum((x-y)**2)
 
-balltree = BallTree(data, metric=squared_euclidean, leaf_size=num_neighbors)
-dists, idxs = balltree.query(data, k=num_neighbors)
+balltree = BallTree(npdata, metric=squared_euclidean, leaf_size=num_neighbors)
+dists, idxs = balltree.query(npdata, k=num_neighbors)
+dists = dists/np.max(dists)
+
+# project data into lower dimension
+projected = torch.rand(num_points, dim)
+
+# find distance from source idx[:,0] to neighbors
+#for start_idx, nbrs in enumerate(idxs):
+#    d = data[start_idx] - data[nbrs]
+nbrs = projected[idxs[:,1:]]
+srcs = projected[idxs[:,0]]
+
+nbrs = nbrs.permute((1,0,2))  
+
+d = srcs - nbrs
+
+# distance from source to non-neighbors
+
+
 #for d in dists:
 #    d[-1]
 # find farthest neighbors
