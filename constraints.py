1ad2de886dde1c20766e6390200b55fd64f8c89b
Bryan Glazer <bryan.glazer@vanderbilt.edu>
2019-09-12 09:58:13 -0500

    First prototype, not really working

--- constraints.py
+++ constraints.py
@@ -9,6 +9,7 @@ from torchvision import transforms
 from torch.autograd import Variable
 import numpy as np
 from sklearn.neighbors import BallTree, DistanceMetric
+from matplotlib import pyplot as plt
 
 # load data
 num_points = 400
@@ -20,7 +21,7 @@ data = datasets.MNIST(root='./data',
                       train=True,
                       transform=transforms.ToTensor(),
                       download=True)
-labels = data.targets[:num_points]
+labels = data.targets[:num_points].numpy()
 data = data.data
 data = data.type(torch.FloatTensor)
 data = Variable(data.view(-1, 28*28))
@@ -30,6 +31,10 @@ npdata = data.numpy()
 #data = Batch(Data(x=data))
 
 # compute knn graph
+def squared_euclidean(x, y):
+    d = np.sum((x-y)**2)
+    return d
+
 balltree = BallTree(npdata, metric=squared_euclidean, leaf_size=num_nbrs)
 dists, idxs = balltree.query(npdata, k=num_nbrs)
 dists = dists/np.max(dists)
@@ -44,43 +49,63 @@ projected = np.random.random((num_points, dim))
 num_iters = 1
 eps = .01
 
-#def step(projected):
-nbrs = projected[idxs[:,1:]]
-srcs = projected[idxs[:,0]]
+def step(projected, i):
+    nbrs = projected[idxs[:,1:]]
+    srcs = projected[idxs[:,0]]
 
-nbrs = nbrs.transpose((1,0,2))
+    nbrs = nbrs.transpose((1,0,2))
 
-d_nbrs = srcs - nbrs
+    d_nbrs = srcs - nbrs
 
-# distance from source to non-neighbors
-num_non_nbrs = 10
+    # distance from source to non-neighbors
+    num_non_nbrs = 10
 
-#non_nbrs = np.ndarray((total_non_nbrs, dim))
-d_non_nbrs = np.ndarray((num_non_nbrs, num_points, dim))
+    #non_nbrs = np.ndarray((total_non_nbrs, dim))
+    d_non_nbrs = np.ndarray((num_non_nbrs, num_points, dim))
 
-for idx_row in idxs:
-    src_idx = idx_row[0]
-    src = projected[src_idx]
+    for idx_row in idxs:
+        src_idx = idx_row[0]
+        src = projected[src_idx]
 
-    p = np.ones(num_points)
-    p = p * 1/(num_points - num_nbrs)
-    p[idx_row] = 0.0
-    random_non_nbrs = np.random.choice(num_points, size=(num_non_nbrs), replace=False, p=p)
-    d_non_nbrs[:,src_idx,:] = src - projected[random_non_nbrs]
-    
-# find farthest neighbors
-d_farthest = d_nbrs[-1,:,:]
-dist_farthest = np.sum(d_nbrs[-1,:,:], axis=1)
+        p = np.ones(num_points)
+        p = p * 1/(num_points - num_nbrs)
+        p[idx_row] = 0.0
+        random_non_nbrs = np.random.choice(num_points, size=(num_non_nbrs), replace=False, p=p)
+        d_non_nbrs[:,src_idx,:] = src - projected[random_non_nbrs]
+        
+    # find farthest neighbors
+    d_farthest = d_nbrs[-1,:,:]
+    dist_farthest = np.sum(d_nbrs[-1,:,:], axis=1)
 
-# calculate gradient
-dist_nbr = np.sum(d_nbrs, axis=2)
-dist_non_nbr = np.sum(d_non_nbrs, axis=2)
+    # calculate gradient
+    dist_nbr = np.sum(d_nbrs, axis=2)
+    dist_non_nbr = np.sum(d_non_nbrs, axis=2)
 
-nbr_mask = dist_nbr < dist_farthest
-non_nbr_mask = dist_non_nbr > dist_farthest
+    # Points in original local neighborhood that are outside the threshold in the projected space
+    nbr_mask = dist_nbr > dist_farthest
+    non_nbr_mask = dist_non_nbr < dist_farthest
 
-grad = np.sum(d_farthest - d_nbrs, axis=0)
+    grad_nbr = d_farthest - d_nbrs
+    grad_nbr[nbr_mask] = 0.0
+    grad_nbr = np.sum(grad_nbr, axis=0)
 
-# optimize wrt constraints
-#projected = projected + grad*eps
-#return projected
+    grad_non_nbr = d_farthest - d_non_nbrs
+    grad_non_nbr[non_nbr_mask] = 0.0
+    grad_non_nbr = np.sum(grad_non_nbr, axis=0)
+
+    grad = grad_nbr + grad_non_nbr
+
+    # optimize wrt constraints
+    up = projected + grad*eps
+
+    return up
+
+    #fig = plt.figure()
+    #ax = fig.add_subplot(1,1,1)
+
+    #up = projected
+    #def plot_step(up):
+    #up = step(up)
+    #ax.clear()
+    #plt.scatter(up[:,0], up[:,1])
+    #return up
