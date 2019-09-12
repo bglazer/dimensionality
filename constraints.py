a47cb4f2724463d53d0e457507eac034062a7e1d
Bryan Glazer <bryan.glazer@vanderbilt.edu>
2019-09-12 09:24:23 -0500

    Adding non_neighbor calculations, plus gradient

--- constraints.py
+++ constraints.py
@@ -12,7 +12,7 @@ from sklearn.neighbors import BallTree, DistanceMetric
 
 # load data
 num_points = 400
-num_neighbors = 10
+num_nbrs = 10
 dim = 2
 #sample_size = 50
 
@@ -26,38 +26,61 @@ data = data.type(torch.FloatTensor)
 data = Variable(data.view(-1, 28*28))
 data = data[:num_points]
 npdata = data.numpy()
-#data.requires_grad = True
 #data = Data(pos=data)
 #data = Batch(Data(x=data))
 
 # compute knn graph
-def squared_euclidean(x, y):
-    return np.sum((x-y)**2)
-
-balltree = BallTree(npdata, metric=squared_euclidean, leaf_size=num_neighbors)
-dists, idxs = balltree.query(npdata, k=num_neighbors)
+balltree = BallTree(npdata, metric=squared_euclidean, leaf_size=num_nbrs)
+dists, idxs = balltree.query(npdata, k=num_nbrs)
 dists = dists/np.max(dists)
 
 # project data into lower dimension
-projected = torch.rand(num_points, dim)
+# TODO better initial projection? T-SNE/UMAP/PCA etc? Maybe PCA, given results in paper?
+projected = np.random.random((num_points, dim))
 
-# find distance from source idx[:,0] to neighbors
+# find distance from source idx[:,0] to neighbors, using data so that gradient can be calculated
 #for start_idx, nbrs in enumerate(idxs):
 #    d = data[start_idx] - data[nbrs]
+num_iters = 1
+eps = .01
+
+#def step(projected):
 nbrs = projected[idxs[:,1:]]
 srcs = projected[idxs[:,0]]
 
-nbrs = nbrs.permute((1,0,2))  
+nbrs = nbrs.transpose((1,0,2))
 
-d = srcs - nbrs
+d_nbrs = srcs - nbrs
 
 # distance from source to non-neighbors
+num_non_nbrs = 10
 
+#non_nbrs = np.ndarray((total_non_nbrs, dim))
+d_non_nbrs = np.ndarray((num_non_nbrs, num_points, dim))
 
-#for d in dists:
-#    d[-1]
+for idx_row in idxs:
+    src_idx = idx_row[0]
+    src = projected[src_idx]
+
+    p = np.ones(num_points)
+    p = p * 1/(num_points - num_nbrs)
+    p[idx_row] = 0.0
+    random_non_nbrs = np.random.choice(num_points, size=(num_non_nbrs), replace=False, p=p)
+    d_non_nbrs[:,src_idx,:] = src - projected[random_non_nbrs]
+    
 # find farthest neighbors
-# write constraints
+d_farthest = d_nbrs[-1,:,:]
+dist_farthest = np.sum(d_nbrs[-1,:,:], axis=1)
+
+# calculate gradient
+dist_nbr = np.sum(d_nbrs, axis=2)
+dist_non_nbr = np.sum(d_non_nbrs, axis=2)
+
+nbr_mask = dist_nbr < dist_farthest
+non_nbr_mask = dist_non_nbr > dist_farthest
+
+grad = np.sum(d_farthest - d_nbrs, axis=0)
+
 # optimize wrt constraints
-
-
+#projected = projected + grad*eps
+#return projected
