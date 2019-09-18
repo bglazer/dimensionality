7ab7bb69b3365fda977535f6ea795efb465da579
Bryan Glazer <bryan.glazer@vanderbilt.edu>
2019-09-18 16:08:03 -0500

    Added stochastic updates

--- constraints.py
+++ constraints.py
@@ -47,10 +47,16 @@ class JaccardGradient():
 
         return ajd/self.num_points
 
+    def step(self, projected, num_updates=None, prev_grad=None, eps=1.0, gamma=.9, verbose=False):
+        if num_updates is None:
+            update_idxs = np.arange(self.num_points)
+            num_updates = self.num_points
+        else:
+            update_idxs = np.random.choice(self.num_points, size=num_updates, replace=False)
 
-    def step(self, projected, prev_grad, eps, gamma, verbose=False):
-        nbrs = projected[self.idxs[:,1:]]
-        srcs = projected[self.idxs[:,0]]
+        idxs = self.idxs[update_idxs]
+        nbrs = projected[idxs[:,1:]]
+        srcs = projected[idxs[:,0]]
 
         nbrs = nbrs.transpose((1,0,2))
 
@@ -59,20 +65,19 @@ class JaccardGradient():
 
         # Calculate distances
         dist_nbrs = np.sqrt(np.sum(d_nbrs**2, axis=2))
-        # find closest non-neighbor in projected space
-        farthest_nbr_idx = np.argmax(dist_nbrs, axis=0)
         # distance to farthest neighbor
         balltree = BallTree(projected, metric=self.metric, leaf_size=self.num_nbrs)
-        farthest_nbr_dist = dist_nbrs[farthest_nbr_idx, np.arange(self.num_points)]
         proj_dists, proj_idxs= balltree.query(srcs, k=self.num_nbrs)
 
         grad = np.zeros((self.num_points, self.dim))
+        if prev_grad is None:
+            prev_grad = np.zeros((self.num_points, self.dim))
+        # TODO somehow switch to vectorized operations?
         # find distance to closest non-neighbor in projected space
         # have to use a for loop because there are a varying number of non neighbors for each point
-        # TODO somehow switch to vectorized operations?
-        # can't use vectorized numpy operations
+        # so can't use vectorized numpy operations
         for i,proj_idx in enumerate(proj_idxs):
-            non_nbr_idxs = np.setdiff1d(proj_idx, self.idxs[i], assume_unique=True)
+            non_nbr_idxs = np.setdiff1d(proj_idx, idxs[i], assume_unique=True)
             if len(non_nbr_idxs) == 0:
                 continue
             # get points inside the radius
@@ -85,7 +90,7 @@ class JaccardGradient():
             farthest_nbr = proj_dists[i,-1] 
             constraints = dist_nbrs[:,i] < farthest_nbr
             # where the constraint is not met, calculate the gradient
-            unmet_idxs = self.idxs[i,1:][~constraints]
+            unmet_idxs = idxs[i,1:][~constraints]
             unmet_nbrs = d_nbrs[:,i][~constraints]
             # np.newaxis is a hack to make dist_nbrs broadcastable
             unmet_dist = dist_nbrs[:,i,np.newaxis][~constraints]
@@ -94,13 +99,14 @@ class JaccardGradient():
             grad[non_nbr_idxs] += d_non_nbrs/dist_non_nbrs
 
             # gradient with respect to source point
-            grad[i] += \
+            grad[update_idxs[i]] += \
                 np.sum(unmet_nbrs/unmet_dist, axis=0) - \
                 np.sum(d_non_nbrs/dist_non_nbrs, axis=0)
 
             
         grad = (grad*eps + prev_grad*gamma)
 
+        #projected[update_idxs] -= grad[update_idxs]
         projected -= grad
 
         if verbose:
@@ -110,16 +116,26 @@ class JaccardGradient():
 
         return projected, grad
 
-    def fit_transform(self, eps=1.0, gamma=.9, num_iters=50, verbose=False):
+    def fit_transform(self, num_updates=None, eps=1.0, gamma=.9, num_iters=50, verbose=False):
         if verbose:
             start_ajd = self.average_jaccard(self.initial_projected)
             
-        prev_grad = np.zeros((self.num_points, self.dim))
-        projected, grad = self.step(self.initial_projected, prev_grad, gamma=gamma, eps=eps, verbose=verbose)
-        for i in range(num_iters):
+        projected, grad = self.step(projected=self.initial_projected, 
+                                    num_updates=num_updates,
+                                    gamma=gamma,
+                                    eps=eps,
+                                    verbose=verbose)
+        prev_grad = grad
+        for i in range(num_iters-1):
             # TODO learning rate decay?
             eps = .99*eps
-            projected, grad = self.step(projected, prev_grad, gamma=gamma, eps=eps, verbose=verbose)
+            if verbose: print(i)
+            projected, grad = self.step(projected=projected,
+                                        num_updates=num_updates,
+                                        prev_grad=prev_grad,
+                                        gamma=gamma,
+                                        eps=eps,
+                                        verbose=verbose)
             prev_grad = grad
 
         if verbose:
@@ -130,15 +146,17 @@ class JaccardGradient():
         return projected, grad
             
 
-    def plot(self, projected, labels=None, grad=None):
-        fig, ax = plt.subplots()
+    def plot(self, projected, labels=None, grad=None, filename=None):
+        fig, ax = plt.subplots(figsize=(20,20))
         plt.scatter(projected[:,0], projected[:,1], c=labels, s=6)
         if grad is not None:
             lines = np.stack((projected,projected+grad)).transpose((1,0,2))
             lc = collections.LineCollection(lines)
             ax.add_collection(lc)
-        plt.show()
-        plt.pause(10)
+        if filename is None:
+            plt.show(block=True)
+        else:
+            plt.savefig(filename)
 
     # TODO fix this
     def animate(self, num_iters, labels):
